9f1b99060b61c4f3c8709ab15dc3fa6e1d0b0939
Rodrigo Lodeiro <rodrigo.lodeiro@live.com>
2017-11-15 00:38:30 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -40,7 +40,7 @@ class SongsController < ApplicationController
     @song.update(params[:song])
     @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
     @song.save
-    
+
     flash[:message] = "Successfully updated song."
     redirect to("/songs/#{@song.slug}")
   end
